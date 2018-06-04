45f4b648d4fdedf12b08f2b81ed5ac1e37babc3c
rohit <inforohitindian@gmail.com>
2018-06-05 00:23:53 +0530

    created get put patch and delete api

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from rest_framework import status
+
+from . import serializers
+
 # Create your views here.
 
 class HelloApiView(APIView):
     """Test API view"""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self,request,format=None):
         """returns a list of APIView feature"""
@@ -18,3 +24,34 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message':'Hello!','an_apiview':an_apiview})
+
+    def post(get,request):
+        """create a hello message with our name"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'hello {0}'.format(name)
+            return Response({'message':message})
+
+        else:
+             return Response(
+                serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk=None):
+        """Handle updating an oject"""
+
+        return Response({'method':'put'})
+
+    def patch(self, request, pk=None):
+        """patch request only update fields required"""
+
+        return Response({'method':'patch'})
+
+
+    def delete(self, request, pk=None):
+        """delete an oject"""
+
+        return Response({'method':'delete'})        
